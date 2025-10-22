03b5a903894d66dfc4da0484160f3de4fc5d2d77
Jordan Gregory-Wallis <j.gregorywallis@gmail.com>
2025-10-22 01:04:20 +0100

    tweaked functionality to open a text editor on click of toast message

--- Toast.cs
+++ Toast.cs
@@ -1,39 +1,60 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
-using ReactiveUI;
 
 public class Toast : Window
 {
+    private string _text;
     public Toast(string text)
     {
+        _text = text;
         Width = 500;
         Height = 200;
+        Cursor = new Avalonia.Input.Cursor(Avalonia.Input.StandardCursorType.Hand);
         CanResize = false;
-        SystemDecorations = SystemDecorations.None;
+        SystemDecorations = SystemDecorations.Full;
         TransparencyLevelHint = [WindowTransparencyLevel.AcrylicBlur, WindowTransparencyLevel.Transparent];
         Background = Brushes.BlueViolet;
         Topmost = true;
+        Icon = new WindowIcon("Assets/icon.png");
+        Title = "Alpha Grab";
+
+        PointerPressed += OnWindowClick;
 
         Content = new StackPanel
         {
             Margin = new Thickness(10),
             Children =
             {
-                new TextBlock { Text = text, TextWrapping = TextWrapping.Wrap, Margin = new Thickness(0,0,0,8) },
-                new Button { Content = "Copy to Clipboard", Command = ReactiveCommand.CreateFromTask(async () =>
-                {
-                    await Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(async () =>
-                    {
-                        var clipboard = GetTopLevel(this)?.Clipboard;
-                        if (clipboard != null)
-                            await clipboard.SetTextAsync(text);
-                    });
-                }) }
+                new TextBlock { Text = "Extracted Text", FontWeight = FontWeight.Bold, HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch },
+                new TextBlock { Text = _text, TextWrapping = TextWrapping.Wrap, Margin = new Thickness(0,0,0,8) },
             }
         };
-
         var screen = Screens.Primary.Bounds;
         Position = new PixelPoint((int)(screen.Width - Width - 20), (int)(screen.Height - Height - 40));
+    }
+    
+    private void OnWindowClick(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+        var width = 800;
+        var height = 200;
+        var inputWindow = new Window()
+        {
+            Width = width,
+            Height = height,
+            Title = "Alpha Grab",
+            Icon = new WindowIcon("Assets/icon.png"),
+            Background = Brushes.BlueViolet,
+            Content = new TextBox
+            {
+                Text = _text,
+                Width = width,
+                Height = height - 100,
+                Background = Brushes.BlueViolet,
+            }
+        };
+        inputWindow.Show();
+
+        this.Close();
     }
 }
